61473f4da4573e9cf17a1876daa090ef6fb2bf68
Greg <kousagi1012@yahoo.com>
2017-09-22 14:55:53 -0700

    react app setup

---
+++ src/components/sidenav.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react' 
+
+export default class SideNav extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className="sideNav">
+        blah
+      </div>
+    )
+  }
+}
